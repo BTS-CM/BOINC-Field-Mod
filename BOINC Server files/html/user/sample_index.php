df41d807051375c934a129f9020358735d8a333c
bug <grctestemail@gmail.com>
2016-08-26 13:47:39 +0100

    Changing front-end
    Crypto integration complete, making the site look better.

--- BOINC Server files/html/user/sample_index.php
+++ BOINC Server files/html/user/sample_index.php
@@ -23,7 +23,7 @@
 // and customizing the header/footer functions in html/project/project.inc
 
 require_once("../inc/util.inc");
-require_once("../inc/news.inc");
+//require_once("../inc/news.inc");
 require_once("../inc/cache.inc");
 require_once("../inc/uotd.inc");
 require_once("../inc/sanitize_html.inc");
@@ -43,11 +43,11 @@ function show_nav() {
     ";
     if ($no_computing) {
         echo "
-            Project-Rain matches BOINC user's CPID to multiple cryptocurrency accounts/addresses to extend project rain (tipping BOINC users) to multiple cryptocurrency platforms.
+            Project-Rain matches BOINC user's CPID to multiple cryptocurrency accounts/addresses to extend 'Project Rain' (tipping BOINC users) to multiple cryptocurrency networks & all BOINC teams.
         ";
     } else {
         echo "
-            Project-Rain matches BOINC user's CPID to multiple cryptocurrency accounts/addresses to extend project rain (tipping BOINC users) to multiple cryptocurrency platforms.
+            Project-Rain matches BOINC user's CPID to multiple cryptocurrency accounts/addresses to extend 'Project Rain' (tipping BOINC users) to multiple cryptocurrency networks & all BOINC teams.
         ";
     }
     echo "
@@ -57,36 +57,57 @@ function show_nav() {
         <li> <a href='https://steemit.com/boinc/@cm-steem/project-rain-distributing-crypto-assets-to-boinc-users-based-on-their-verified-boinc-computation'>Project-Rain Steem post</a>.</li>
         <li> Got an idea for a BOINC project? <a href='https://steemit.com/gridcoin/@cm-steem/brainstorming-new-boinc-projects-anyone-can-create-a-project-and-reward-their-users-with-gridcoin'>Post your ideas in the Brainstorming thread</a> on Steem!</li>
         </ul>
-        <p>Currently supported cryptocurrencies:</p>
-        <ul>
-            <li>Gridcoin, Bitshares, STEEM, Ethereum (ETH), Ethereum Classic (ETC), Golem, NXT, Ardor, Hyperledger, Waves, Peershares, Omnilayer and Counterparty.</li>
-        </ul>
+        <h4>Currently supported cryptocurrencies:</h4>
+        <p>
+            <a href='https://www.gridcoin.us'>Gridcoin</a>, <a href='https://www.bitshares.org'>Bitshares</a>, <a href='https://steemit.com/'>STEEM</a>, <a href='https://www.ethereum.org/'>Ethereum (ETH)</a>, <a href='https://ethereumclassic.github.io/'>Ethereum Classic (ETC)</a>, <a href='http://www.golemproject.net/'>Golem</a>, <a href='https://nxt.org/'>NXT</a>, <a href='https://ardorplatform.org/'>Ardor</a>, <a href='https://intelledger.github.io/introduction.html'>Hyperledger: Sawtooth Lake (Intel)</a>, <a href='https://hyperledger-fabric.readthedocs.io/en/latest/'>Hyperledger: Fabric (IBM)</a>, <a href='https://wavesplatform.com/'>Waves</a>, <a href='www.peershares.net'>Peershares</a>, <a href='http://www.omnilayer.org'>Omnilayer</a> and <a href='https://counterparty.io'>Counterparty</a>.
+        </p>
+        <h4>Why is cryptocurrency 'x/y/z' not included?</h4>
+        <p>
+            We mainly added cryptocurrencies that are '2nd gen', specifically those which allow the issuance of new crypto assets within their network (Bitshares/Ardor/Waves/etc). Cryptocurrencies such as Bitcoin are not supported due to the fact that project-rain would be classed as 'dust' and would not be sufficient precipitation for a 'project rain' against tens of thousands of users.
+        </p>
+        <h4>
+            Can you add cryptocurrency 'x/y/z'?
+        </h4>
+        <p>
+            Negative, once this project is live the cryptocurrencies selected will be set in stone. If you wish to create 'Project Rain' capabilities for your cryptocurrency, you can fork the <a href=''>Project-Rain Github repo</a> and create your own BOINC project. Alternatively, you could convert your cryptocurrency to Gridcoin/ETH/ETC/Steem/Bitshares assets to create a project rain on behalf of your cryptocurrency.
+        </p>
+        <h4>
+            Is this not detrimental to Gridcoin?
+        </h4>
+        <p>
+            I do not believe so, this will allow Gridcoin users to recieve multiple crypto assets for their verified BOINC work, of which could be sold for Gridcoin. Merged mining does not damage the cryptocurrencies participating in merged mining, it strengthens their cryptocurrency platforms and gets their asset into more users hands - by the same logic we should not fear 'double dipping' of BOINC rewards. Lastly, this is a centralized method of issuing crypto assets, Gridcoin has a decentralized reward mechanism.
+        </p>
+        <h4>
+            ATTENTION:
+        </h4>
+        <p>
+            In order for your Project-Rain CPID to match your BOINC project CPIDs, you <b>must</b> add your Project-Rain account to your BOINC manager client alongside the running projects, otherwise your CPIDs will not match & you will be skipped for project rain!
+        </p>
         <h2 class=headline>Join the project!</h2>
         <ul>
     ";
     if ($no_computing) {
         if (!$no_web_account_creation && !$disable_acct) {
             echo "
-                <li> Want to potentially recieve project rain across multiple crypto platforms? <a href=\"create_account_form.php\">Create an account</a>!
+                <li> Want to potentially recieve 'project rain' across multiple crypto networks? <a href=\"create_account_form.php\">Create an account today</a>!
             ";
         } else {
             echo "<li> This project is not currently accepting new accounts. Something has gone wrong, contact CM!";
         }
     } else {
         echo "
-            <li><a href=\"info.php\">".tra("Read our rules and policies")."</a>
+            <li><a href=\"info.php\">".tra("Read our rules and policies")."</a></li>
         ";
         if (0) {
             echo "<li>";
             show_button("register.php", "Join", null, "btn btn-green");
         } else {
-            echo "<li> <a href=https://boinc.berkeley.edu/download.php>Download</a> and run BOINC.
-                <li> Choose Add Project
+            echo "<li> <a href=https://boinc.berkeley.edu/download.php>Download</a> and run BOINC.</li>
+                <li> Choose Add Project & enter https://www.project-rain.co.uk/boincserver/ into the URL field.</li>
             ";
         }
         echo "
-            <li> If you have any problems,
-                <a target=\"_new\" href=\"https://boinc.berkeley.edu/wiki/BOINC_Help\">get help here</a>.
+            <li> If you have any problems with project-rain, do not hesitate to contact CM via email/IRC/Steem.</li>
         ";
     }
     echo "
@@ -127,7 +148,7 @@ function show_nav() {
         ";
     }
     echo "
-        <li><a href=\"stats.php\">Statistics</a>
+        <li><a href=\"stats.php\">Statistics for aiding execution of 'project rain'</a>
         <li><a href=language_select.php>Languages</a>
         </ul>
         </div>
@@ -180,7 +201,7 @@ if ($stopped) {
 echo "
     <p>
     <a href=\"https://boinc.berkeley.edu/\"><img align=\"middle\" border=\"0\" src=\"img/pb_boinc.gif\" alt=\"Powered by BOINC\"></a></br>
-    <a href=\"https://www.digitalocean.com/?refcode=b86d3e55f889\"><img src=\"img/DigitalOcean.png\" alt=\"DigitalOcean\"/></a>
+    <a href=\"https://www.digitalocean.com/?refcode=b86d3e55f889\"><img src=\"img/DigitalOcean.png\" alt=\"DigitalOcean\" style=\"padding-top:10px;\"/></a>
     </p>
     </td>
 ";
@@ -197,13 +218,13 @@ if (!$stopped && !DISABLE_PROFILES) {
     }
 }
 
-echo "
-    <tr><td class=news>
-    <h2 class=headline>News</h2>
-    <p>
-";
-include("motd.php");
-show_news(0, 5);
+//echo "
+//    <tr><td class=news>
+//   <h2 class=headline>News</h2>
+//    <p>
+//";
+//include("motd.php");
+//show_news(0, 5);
 echo "
     </td>
     </tr></table>
