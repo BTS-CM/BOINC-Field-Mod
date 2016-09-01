0e54608363d7a267247a7e73d0c613bcaa7a90f8
bug <grctestemail@gmail.com>
2016-09-01 01:41:12 +0100

    bug fixes?

--- BOINC Server files/db/boinc_db.cpp
+++ BOINC Server files/db/boinc_db.cpp
@@ -131,8 +131,6 @@ DB_HOST_APP_VERSION::DB_HOST_APP_VERSION(DB_CONN* dc) :
     DB_BASE("host_app_version", dc?dc:&boinc_db){}
 DB_USER_SUBMIT::DB_USER_SUBMIT(DB_CONN* dc) :
     DB_BASE("user_submit", dc?dc:&boinc_db){}
-DB_PROJECT_RAIN::DB_PROJECT_RAIN(DB_CONN* dc) :
-    DB_BASE("project_rain", dc?dc:&boinc_db){}
 DB_STATE_COUNTS::DB_STATE_COUNTS(DB_CONN* dc) :
     DB_BASE("state_counts", dc?dc:&boinc_db){}
 DB_TRANSITIONER_ITEM_SET::DB_TRANSITIONER_ITEM_SET(DB_CONN* dc) :
@@ -186,7 +184,6 @@ DB_ID_TYPE DB_PLATFORM::get_id() {return id;}
 DB_ID_TYPE DB_APP::get_id() {return id;}
 DB_ID_TYPE DB_APP_VERSION::get_id() {return id;}
 DB_ID_TYPE DB_USER::get_id() {return id;}
-DB_ID_TYPE DB_PROJECT_RAIN::get_id() {return id;}
 DB_ID_TYPE DB_TEAM::get_id() {return id;}
 DB_ID_TYPE DB_HOST::get_id() {return id;}
 DB_ID_TYPE DB_WORKUNIT::get_id() {return id;}
